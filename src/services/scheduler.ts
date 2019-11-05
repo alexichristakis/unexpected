b5a8e8671d190c98e0300401c01b356f31ef96ef
Alexi Christakis <alexi.christakis@gmail.com>
2019-11-04 21:15:51 -0500

    more agenda stuff

--- src/services/scheduler.ts
+++ src/services/scheduler.ts
@@ -1,6 +1,8 @@
 import { Inject, Service } from "@tsed/common";
 import { MongooseService } from "@tsed/mongoose";
+import { $log } from "ts-log-debug";
 import Agenda from "agenda";
+import moment, { Moment } from "moment-timezone";
 
 import { UserService } from "./user";
 import { NotificationService } from "./notification";
@@ -9,6 +11,8 @@ export enum AgendaJobs {
   GENERATE_NOTIFICATIONS = "GENERATE_NOTIFICATIONS",
   SEND_NOTIFICATION = "SEND_NOTIFICATION"
 }
+
+$log.name = "Agenda";
 
 @Service()
 export class SchedulerService {
@@ -26,44 +30,66 @@ export class SchedulerService {
   async $afterRoutesInit() {
     this.agenda = new Agenda().mongo(this.mongooseService.get()!.connection.db, "jobs");
 
-    this.agenda.define(AgendaJobs.SEND_NOTIFICATION, args => {
-      console.log("SENDING NOTIFICATION");
-      const { to } = args.attrs.data;
-      this.notificationService.notifyUserModel(to, "time to take & share a photo");
+    this.agenda.processEvery("5 minutes");
+
+    this.agenda.on("complete", job => {
+      $log.debug(`Job ${job.attrs.name} finished`);
     });
 
-    this.agenda.define(AgendaJobs.GENERATE_NOTIFICATIONS, async () => {
-      console.log("GENERATING NOTIFICATIONS");
-      const users = await this.userService.getAll([
-        "_id",
-        "timezone",
-        "deviceOS",
-        "deviceToken",
-        "firstName",
-        "lastName"
-      ]);
-
-      users.forEach(user => {
-        const { timezone } = user;
-        const time = this.generateTime(timezone);
-
-        this.agenda.schedule("every hour", AgendaJobs.SEND_NOTIFICATION, { to: user });
-      });
-    });
-
-    this.agenda.processEvery("10 minutes");
-
-    await new Promise((resolve, reject) => {
+    await new Promise(resolve => {
       this.agenda.on("ready", async () => {
+        await this.agenda.purge();
+
+        this.agenda.define(AgendaJobs.SEND_NOTIFICATION, args => {
+          const { to } = args.attrs.data;
+          this.notificationService.notifyUserModel(to, "time to take & share a photo");
+        });
+
+        this.agenda.define(AgendaJobs.GENERATE_NOTIFICATIONS, async () => {
+          const users = await this.userService.getAll([
+            "_id",
+            "timezone",
+            "deviceOS",
+            "deviceToken",
+            "firstName",
+            "lastName"
+          ]);
+
+          users.forEach(user => {
+            const { timezone } = user;
+
+            for (let i = 0; i < 10; i++) {
+              const time = this.generateTime(timezone);
+              $log.debug(
+                `notification for ${user.firstName} at: ${time.format(
+                  "dddd, MMMM Do YYYY, h:mm:ss a"
+                )}`
+              );
+
+              this.agenda.schedule(time.toDate(), AgendaJobs.SEND_NOTIFICATION, { to: user });
+            }
+          });
+        });
+
         await this.agenda.start();
         await this.scheduleNotificationGeneration();
+
         resolve();
       });
     });
   }
 
-  generateTime = (timezone: string): Date => {
-    return new Date();
+  private generateTime = (timezone: string = "America/New_York"): Moment => {
+    const start = moment.tz(moment().add(1, "day"), timezone).hour(10);
+    const end = start.clone().hour(22);
+
+    const endTime = +end;
+
+    const randomNumber = (to: number, from = 0) => Math.floor(Math.random() * (to - from) + from);
+
+    const startTime = +start;
+
+    return moment(randomNumber(endTime, startTime));
   };
 
   scheduleNotificationGeneration = async () => {
