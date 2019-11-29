ba4c380441a6ea6d96efe1ba2bd209bd2e454341
Alexi Christakis <alexi.christakis@gmail.com>
2019-11-29 13:39:28 -0600

    setup codepush

--- ios/unexpected/AppDelegate.m
+++ ios/unexpected/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <CodePush/CodePush.h>
 #import <RNCPushNotificationIOS.h>
 
 @implementation AppDelegate
@@ -34,11 +35,11 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  #if DEBUG
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    return [CodePush bundleURL];
+  #endif
 }
 
 // Required to register for notifications
