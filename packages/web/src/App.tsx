3a8c5553874e1d551ff7aee6a57a2fbfcf8cad6b
Alexi Christakis <alexi.christakis@gmail.com>
2020-01-28 01:23:25 -0500

    add

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>expect photos soon</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
