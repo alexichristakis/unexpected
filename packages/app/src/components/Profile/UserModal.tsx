c2ea49af2ca3c56d01c535346e1061220b0ecd71
Alexi Christakis <alexi.christakis@gmail.com>
2020-02-13 01:27:44 -0500

    add to profile

--- packages/app/src/components/Profile/UserModal.tsx
+++ packages/app/src/components/Profile/UserModal.tsx
@@ -9,18 +9,18 @@ import {
 import { User } from "@unexpected/global";
 
 export interface UserModalProps {
-  type: "friends" | "requests";
+  type?: "friends" | "requests";
   modalRef: React.Ref<ModalListRef>;
   onPressUser: (user: User) => void;
   data: User[];
 }
 
 export const UserModal: React.FC<UserModalProps> = React.memo(
-  ({ type, data, onPressUser, modalRef }) => {
+  ({ type = "friends", data, onPressUser, modalRef }) => {
     const renderUserRow = (item: User, index: number) => (
       <>
-        {index ? <ItemSeparator /> : null}
-        <UserRow key={index} user={item} onPress={onPressUser} />
+        {index ? <ItemSeparator key={`separator-${index}`} /> : null}
+        <UserRow key={`user-${index}`} user={item} onPress={onPressUser} />
       </>
     );
 
